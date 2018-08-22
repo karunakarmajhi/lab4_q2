a1f91eb7a3bd09848189825071e25426628963ce
karunakarmajhi <42205167+karunakarmajhi@users.noreply.github.com>
2018-08-22 17:36:11 +0530

    Add files via upload

---
+++ lab4_q2.cpp
@@ -0,0 +1,26 @@
+//write a C++ program to enter temperature in celsius and convert it into fahrenheit
+
+//library
+#include<iostream>
+using namespace std;
+
+//writing of the main function
+int main(){
+
+//declaration of the variables
+float a;
+
+//assigning values to the variables
+cout << "enter temperature in celsius ";
+
+//reading out the values
+cin >> a;
+cout << "the entered value is " << a;
+
+//process
+cout << "temperature in fahrenheit is " << (b*1.8)+32 << "c" << ".\n";
+
+//end statement
+return 0;
+}
+
